f76878433eca99afa2a628a1f9fbe9cb98635a03
Diego Amicabile <diego.amicabile@amicabile.com>
2021-05-02 02:33:22 +0200

    Check md

--- p/find_associations.py
+++ p/find_associations.py
@@ -4,7 +4,8 @@ import re
 dir_mds = 'C:\Users\diego\ck3aars\ck3-murchad\p'
 ppl_file = 'C:\Users\diego\ck3aars\ck3-murchad\people.md'
 history_dirs = 'C:\Users\diego\ck3aars\ck3-murchad\h'
-history_ffs = ['1227.md']
+history_ffs = ['1249_b.md']
+history_oos = ['1249_b_o.md']
 
 file_names = []  
 for file_name in os.listdir(dir_mds):
@@ -24,5 +25,17 @@ zz_all = zz_groups[:len(file_names)]
 
 zz_st = zip(file_names, zz_all)
 
-for zz_s in zz_st:
-    print(zz_s)
+
+for history_ff, history_oo in zip(history_ffs, history_oos):
+    file_to_ana = os.path.join(history_dirs, history_ff)
+    file_to_w = os.path.join(history_dirs, history_oo)
+    for zz_s in zz_st:   
+        with open(file_to_ana, 'r') as fa:
+            file_source = fa.read()
+            old, new = zz_s[1], '..\p\{}'.format(zz_s[0])
+            print('replacing {} with {}'.format(old, new))
+            replace_file = file_source.replace(old, new)
+            
+        with open(file_to_ana, 'w') as fw:
+            fw.write(replace_file)
+    
